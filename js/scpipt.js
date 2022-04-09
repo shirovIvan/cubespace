a3716a3a45263cfe35566edb04ae1abb07d7466e
shirovIvan <102826124+shirovIvan@users.noreply.github.com>
2022-04-09 21:01:13 +0500

    Add files via upload

---
+++ js/scpipt.js
@@ -0,0 +1,30 @@
+const iconMenu = document.querySelector('.menu__icon');
+if(iconMenu){
+    const bodyMenu = document.querySelector('.menu__body');
+    iconMenu.addEventListener("click", function(e) {
+        document.body.classList.toggle('_lock')
+        iconMenu.classList.toggle('_active')
+        bodyMenu.classList.toggle('_active')
+    })
+}
+
+const menuLinks = document.querySelectorAll('.menu__link[data-goto]');
+if(menuLinks.length > 0){
+    menuLinks.forEach(menuLink => {
+        menuLink.addEventListener("click", onMenuLinkClick);
+    })
+    function onMenuLinkClick(e) {
+        const menuLink = e.target;
+        if(menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)){
+            const gotoBlock = document.querySelector(menuLink.dataset.goto);
+            const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset - document.querySelector('header').offsetHeight;
+            
+            window.scrollTo({
+                top: gotoBlockValue,
+                behavior: 'smooth'
+            })
+            e.preventDefault();
+
+        }
+    }
+}
